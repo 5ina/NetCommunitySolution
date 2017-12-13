14baa160b752aff01ee3423f7e0ef376a0fcf9c9
kelvin <kelvin@DESKTOP-K48RVUM>
2017-12-13 10:11:29 +0800

    去掉重复路由

--- NetCommunitySolution.Web/Areas/Operate/OperateAreaRegistration.cs
+++ NetCommunitySolution.Web/Areas/Operate/OperateAreaRegistration.cs
@@ -15,11 +15,6 @@ namespace NetCommunitySolution.Web.Areas.Operate
         public override void RegisterArea(AreaRegistrationContext context) 
         {
             context.MapRoute(
-                "Operate_default",
-                "Operate/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
-            context.MapRoute(
                 name: "Operate_default",
                 url: "Operate/{controller}/{action}/{id}",
                 defaults: new { action = "Index", Controller = "Home", Area = "Operate", id = UrlParameter.Optional }                
